ee77645ebedb7613195b86850b11f208994f9cbe
Sairaj <sairajjadhav716@gmail.com>
2026-08-05 18:24:14 +0530

    Add Student model class

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,8 +1,31 @@
 package model;
-
 public class Student {
-
-    public static void main(String[] args) {
-
+    private int studentId;
+    private String name;
+    private String email;
+    private String branch;
+    private double cgpa;
+    public Student(int studentId, String name, String email,
+                   String branch, double cgpa) {
+        this.studentId = studentId;
+        this.name = name;
+        this.email = email;
+        this.branch = branch;
+        this.cgpa = cgpa;
+    }
+    public int getStudentId() {
+        return studentId;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getBranch() {
+        return branch;
+    }
+    public double getCgpa() {
+        return cgpa;
     }
 }
