b47efaff8b30dd17650e974647684f70b1e3402f
Sairaj <sairajjadhav716@gmail.com>
2026-08-05 18:16:51 +0530

    Initial project structure for Student Placement Management System

---
+++ src/model/Application.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/model/Skills.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Skills {
+
+    public static void main(String[] args) {
+
+    }
+}
